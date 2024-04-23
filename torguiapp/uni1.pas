d732a5126ab8719092f2d349c7506887ee4d3909
Lana <112628962+svetlanatad@users.noreply.github.com>
2024-04-23 13:21:08 +0400

    Works, change exit node before checking

--- torguiapp/uni1.pas
+++ torguiapp/uni1.pas
@@ -1,4 +1,3 @@
-
 unit Unit1;
 
 {$mode objfpc}{$H+}
@@ -6,8 +5,8 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ComCtrls,
-  Menus, IdHTTP, IdSocks, IdIOHandlerStack; // Include IdIOHandlerStack unit
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ComCtrls, StdCtrls,
+  IdHTTP, IdAuthentication;
 
 type
 
@@ -15,14 +14,13 @@ type
 
   TForm1 = class(TForm)
     Button1: TButton;
-    Edit1: TEdit;
-    IdHTTP1: TIdHTTP;
-    Label1: TLabel;
-    MainMenu1: TMainMenu;
+    Button2: TButton;
     StatusBar1: TStatusBar;
-    ToggleBox1: TToggleBox;
+    IdHTTP1: TIdHTTP;
     procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+
   private
 
   public
@@ -33,53 +31,115 @@ var
   Form1: TForm1;
 
 implementation
+uses fpjson, IdSSL, IdSSLOpenSSL, IdTCPClient, IdIOHandlerStack, IdGlobal, IdSocks;
 
-uses
-  fpjson;
 
 {$R *.lfm}
 
 { TForm1 }
 
-procedure TForm1.FormCreate(Sender: TObject);
-begin
-  Form1.Name:= 'aaa';
-end;
-
 procedure TForm1.Button1Click(Sender: TObject);
 var
-  HTTP: TIdHTTP;
-  Response: string;
-  JSON: TJSONObject;
-  SocksInfo: TIdSocksInfo;
-  IOHandlerStack: TIdIOHandlerStack;
+    HTTP: TIdHTTP;
+    Response, city, country: string;
+    JSON: TJSONObject;
+    SocksInfo: TIdSocksInfo;
+    SSLHandler: TIdSSLIOHandlerSocketOpenSSL;
+    IOHandlerStack: TIdIOHandlerStack;
 begin
-  HTTP := TIdHTTP.Create(nil);
-  SocksInfo := TIdSocksInfo.Create(nil);
-  IOHandlerStack := TIdIOHandlerStack.Create(HTTP);
-  try
-    SocksInfo.Host := '127.0.0.1';
-    SocksInfo.Port := 9050;
-    IOHandlerStack.TransparentProxy := SocksInfo;
-    HTTP.IOHandler := IOHandlerStack;
 
+  HTTP := TIdHTTP.Create(nil);
+    SocksInfo := TIdSocksInfo.Create(nil);
+    IOHandlerStack := TIdIOHandlerStack.Create(nil);
     try
-      Response := HTTP.Get('http://ipinfo.io/json'); 
-      JSON := TJSONObject(GetJSON(Response));
+      SocksInfo.Host := '127.0.0.1';
+      SocksInfo.Port := 9050;
+      SocksInfo.Version := svSocks5;
+
+      IOHandlerStack.TransparentProxy := SocksInfo;
+      // for http
+      //IOHandlerStack.Handler := SocksInfo;
+      // for https
+      // SSLHandler := TIdSSLIOHandlerSocketOpenSSL.Create(nil);
+      // IOHandlerStack.Handler := SSLHandler;
+      // SSLHandler.TransparentProxy := SocksInfo;
+
+      HTTP.IOHandler := IOHandlerStack;
+
+      HTTP.Request.UserAgent := 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36';
+
+    // Optionally set other headers
+    //HTTP.Request.Accept := 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8';
+    //HTTP.Request.AcceptLanguage := 'en-US,en;q=0.5';
+    //HTTP.Request.AcceptEncoding := 'gzip, deflate';
+    //HTTP.Request.UserAgent := 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36';
+    //HTTP.Request.Accept := 'application/json'; // Since you're expecting JSON response
+    //HTTP.Request.Connection := 'keep-alive';
+
       try
-        ShowMessage('Location: ' + JSON.Get('city', '') + ', ' + JSON.Get('country', ''));
-      finally
-        JSON.Free;
+        //Response := HTTP.Get('http://ipinfo.io/json?token=YOURTOKEN');
+        //Response := HTTP.Get('http://ipinfo.io/46.3?token=YOURTOKEN');
+        Response := HTTP.Get('http://ipinfo.io?token=REPLACE WITH YOUR TOKEN AFTER REGISTERING');
+        JSON := TJSONObject(GetJSON(Response));
+        try
+          ShowMessage('Location: ' + JSON.Get('city', '') + ', ' + JSON.Get('country', ''));
+          city := JSON.Get('city', ''); country := JSON.Get('country', '');
+          //StatusBar1.Caption := 'Location: ' + city + ', ' + country;
+          StatusBar1.Panels[0].Text := 'Location: ' + city + ', ' + country ;
+
+        finally
+          JSON.Free;
+        end;
+      except
+        on E: Exception do
+          ShowMessage('Failed to retrieve location: ' + E.Message);
       end;
-    except
-      on E: Exception do
-        ShowMessage('Failed to retrieve location: ' + E.Message);
+    finally
+      HTTP.Free;
+      IOHandlerStack.Free;
+      SocksInfo.Free;
+
+    end;
+end;
+
+procedure RenewTorCircuit;
+var
+  TCPClient: TIdTCPClient;
+begin
+  TCPClient := TIdTCPClient.Create(nil);
+  try
+    TCPClient.Host := '127.0.0.1';
+    TCPClient.Port := 9051; 
+    TCPClient.Connect;
+    try
+      TCPClient.IOHandler.WriteLn('AUTHENTICATE "REPLACE WITH YOUR PASSWORD"');
+      TCPClient.IOHandler.WriteLn('SIGNAL NEWNYM');
+    finally
+      TCPClient.Disconnect;
     end;
   finally
-    HTTP.Free;
-    SocksInfo.Free;
-    IOHandlerStack.Free;
+    TCPClient.Free;
   end;
 end;
 
-end.              
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  RenewTorCircuit
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  Button1.Caption:= 'check';
+  Button2.Caption:= 'change tor exit node';
+  if StatusBar1.Panels.Count = 0 then
+  begin
+    with StatusBar1.Panels.Add do
+    begin
+      Width := 200; 
+      Text := '';   
+    end;
+  end;
+end;
+end.
+
+
